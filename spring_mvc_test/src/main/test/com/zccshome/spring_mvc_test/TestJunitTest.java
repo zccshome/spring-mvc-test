5c4fd93d2bd48c63111bf416833a679ae9abd8a7
zccshome <zijinhuakaile@sina.com>
2013-08-13 22:59:33 +0800

    Add junit test.

---
+++ spring_mvc_test/src/main/test/com/zccshome/spring_mvc_test/TestJunitTest.java
@@ -0,0 +1,10 @@
+package com.zccshome.spring_mvc_test;
+
+import org.junit.Test;
+
+public class TestJunitTest {
+	@Test
+	public void junitTest() {
+		System.out.println("Test Junit test!");
+	}
+}
